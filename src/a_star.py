727b4df74f69746625d012c89b668b38e42f5f91
frnzoshi-tech <75726616+frnzoshi-tech@users.noreply.github.com>
2021-07-22 15:35:23 +0700

    update func + add comment

--- src/a_star.py
+++ src/a_star.py
@@ -8,23 +8,24 @@ class AStar:
         self.solution = None
 
     def get_solution(self):
-        return [s.position for s in self.solution]
+        return [s.current_position for s in self.solution]
 
     def is_puzzle_solvable(self):
         return self.start.is_solvable()
 
-    def _calculate_new_heuristic(self, move, end_node):
+    def calculate_new_heuristic(self, move, end_node):
         return move.manhattan_dist() - end_node.manhattan_dist()
 
     def do_algorithm(self):
         queue = [[self.start.manhattan_dist(), self.start]]
         expanded = []
+
         n_nodes_expanded = 0
         path = None
-
         while queue:
             i = 0
             for j in range(1, len(queue)):
+                # Bandingin manhattan_dist
                 if queue[i][0] > queue[j][0]:
                     i = j
 
@@ -32,17 +33,17 @@ class AStar:
             queue = queue[:i] + queue[i + 1:]
             end_node = path[-1]
 
-            if end_node.position == end_node.end_position:
+            if end_node.current_position == end_node.end_position:
                 break
-            if end_node.position in expanded:
+            if end_node.current_position in expanded:
                 continue
 
             for move in end_node.possible_moves():
-                if move.position in expanded:
+                if move.current_position in expanded:
                     continue
-                new_path = [path[0] + self._calculate_new_heuristic(move, end_node)] + path[1:] + [move]
+                new_path = [path[0] + self.calculate_new_heuristic(move, end_node)] + path[1:] + [move]
                 queue.append(new_path)
-                expanded.append(end_node.position)
+                expanded.append(end_node.current_position)
 
             n_nodes_expanded += 1
 
